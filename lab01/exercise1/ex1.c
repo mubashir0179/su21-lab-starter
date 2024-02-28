bf70461fbdc6fcecda9b395844b9b900d8e1f14f
Syed Mubashir Hussain <mubashirkazmi14@gmail.com>
2024-02-28 17:25:16 +0500

    Lab 1 Exercise 1, Part 1 and Part 2 Completed  Changes to be committed: 	modified:   ex1.c 	modified:   test_ex1.c

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -7,13 +7,40 @@ There are two different ways to iterate through a string.
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
     /* TODO: implement num_occurances */
-    return 0;
+
+int count = 0;
+for (int i=0; str[i] !='\0'; i++){
+if (str[i] == letter) {
+count++;
+}
+}
+    return count;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
 Each sequence will end with a NULL terminator and will have up to 20 nucleotides.
 All letters will be upper case. */
-void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
+void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
+    // Initialize counts to zero
+    (*dna_seq).A_count = 0;
+    (*dna_seq).C_count = 0;
+    (*dna_seq).G_count = 0;
+    (*dna_seq).T_count = 0;
+
+    // Iterate through each character in the DNA sequence
+    for (int i = 0; i < strlen((*dna_seq).sequence); i++) {
+        // Check the current character
+        if ((*dna_seq).sequence[i] == 'A')
+            (*dna_seq).A_count++; // Increment A count
+        else if ((*dna_seq).sequence[i] == 'C')
+            (*dna_seq).C_count++; // Increment C count
+        else if ((*dna_seq).sequence[i] == 'G')
+            (*dna_seq).G_count++; // Increment G count
+        else
+            (*dna_seq).T_count++; // Increment T count
+    }
+
+    // The function does not return any value (void)
     return;
 }
